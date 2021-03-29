24166f69434f4634c039d42320c805a4a852f4e8
winterds <happy_to_win@hotmail.com>
2021-03-29 23:07:17 +0700

    update index.js and build.yml

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Lab 9 Done !!'))
 
 app.listen(port, "0.0.0.0", () => console.log(`Web app listening on port ${port}!`))
